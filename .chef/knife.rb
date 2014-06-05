f6f854a6de6a9bb22774b77b099e6eb7dfe33c88
andyzhu <andy.zhu@citrix.com>
2014-06-05 16:38:01 -0700

    change the knife.rb to reflect change the directory of the chef repo

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,11 @@
+
+current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                'andy'
-client_key               'c:/users/andy/documents/github/chef-repo/.chef/andy.pem'
+client_key               'c:/users/andy/documents/github/AO-chef/.chef/andy.pem'
 validation_client_name   'chef-validator'
-validation_key           'C:/Users/andy/Documents/GITHUB/chef-repo/.chef/chef-validator.pem'
+validation_key           'c:/users/andy/documents/github/AO-chef/.chef/chef-validator.pem'
 chef_server_url          'https://ao-chef.cloudapp.net'
-syntax_check_cache_path  'c:/users/andy/documents/github/chef-repo/.chef/syntax_check_cache'
-cookbook_path [ 'c:/users/andy/documents/github/chef-repo/cookbooks' ]
+syntax_check_cache_path  'c:/users/andy/documents/github/AO-chef/.chef/syntax_check_cache'
+cookbook_path [ 'c:/users/andy/documents/github/AO-chef/cookbooks' ]
