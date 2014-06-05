5db677701f9b1868e3c1e4e725f5374ffabc1442
andyzhu <andy.zhu@citrix.com>
2014-06-05 15:16:58 -0700

    initial set up of own chef depo

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'andy'
+client_key               'c:/users/andy/documents/github/chef-repo/.chef/andy.pem'
+validation_client_name   'chef-validator'
+validation_key           'C:/Users/andy/Documents/GITHUB/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://ao-chef.cloudapp.net'
+syntax_check_cache_path  'c:/users/andy/documents/github/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ 'c:/users/andy/documents/github/chef-repo/cookbooks' ]
